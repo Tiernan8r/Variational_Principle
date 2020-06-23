caf71379d9bea40f0f80b61ead6d22d135904fa4
Tiernan8r <tiernan8r@pm.me>
2020-06-23 16:42:07 +0100

    Implemented logging in laplacian.py

--- src/calculus/laplacian.py
+++ src/calculus/laplacian.py
@@ -1,6 +1,8 @@
 from scipy.sparse import diags
 import numpy as np
 
+import logging
+
 
 def _partial_derivative_matrix(D: int, N: int, axis_number: int, dr: float) -> np.ndarray:
     """
@@ -11,19 +13,29 @@ def _partial_derivative_matrix(D: int, N: int, axis_number: int, dr: float) -> n
     :param dr: The grid spacing in the system.
     :return: The second order central difference derivative sparse matrix along the given axis.
     """
+
+    logger = logging.getLogger(__name__)
+    logger.info("Generating a second derivative matrix for %d dimensions of size %d, along axis %d", D, N, axis_number)
+
+    logger.info("Constraining axis number to total number of dimensions")
     # cap axis_number in range to prevent errors.
     axis_number %= D
 
     # Determine the number of the cell grids that need to be repeated along to populate the matrix
     num_cells = D - (axis_number + 1)
+
+    logger.info("Generating second derivative stencil")
     # The general pattern for a derivative matrix along the axis: axis_number, for a num_axes number of
     # dimensions, each of length N
     diagonals = [[-2] * N ** D,
                  (([1] * N ** axis_number) * (N - 1) + [0] * N ** axis_number) * N ** num_cells,
                  (([1] * N ** axis_number) * (N - 1) + [0] * N ** axis_number) * N ** num_cells]
+
+    logger.info("Generating second derivative diagonal matrix")
     # Create a sparse matrix for the given diagonals, of the desired size.
     D_n = diags(diagonals, [0, -N ** axis_number, N ** axis_number], shape=(N ** D, N ** D))
 
+    logger.info("Scaling by grid spacing")
     # return the matrix, factored by the grid spacing as required by the central difference formula
     return D_n * (dr ** -2)
 
@@ -35,6 +47,9 @@ def generate_laplacian(D: int, N: int, dr: float):
     :param N: The size of each dimension.
     :param dr: The grid spacing in the system.
     """
+
+    logger = logging.getLogger(__name__)
+    logger.info("Generating Laplacian matrix operator for system of %d dimensions, sized %d", D, N)
 
     # Initially set DEV2 to be undefined.
     laplacian = None
@@ -49,6 +64,9 @@ def generate_laplacian(D: int, N: int, dr: float):
         # otherwise add it, as matrix multiplication is distributive (ie differentiation is distributive)
         else:
             laplacian += D_n
+
+    logger.info("DONE generating Laplacian.")
+    logger.info("Setting global variable.")
 
     global DEV2
     DEV2 = laplacian
