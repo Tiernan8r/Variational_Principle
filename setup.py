4f9bdeb5f8bcac33f78bc77e8f2ebb70f8ea7f33
Tiernan8r <tiernan8r@pm.me>
2020-06-24 10:06:53 +0100

    Added setup.py for packaging: * As a requirement refactored src -> variational_principle

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='variational_principle',
+      version='1.1',
+      description='The Variational Principle method for computing bound energy eigenstates.',
+      url='https://github.com/Tiernan8r/Variational-Principle',
+      author='Tiernan8r',
+      author_email='tiernan8r@pm.me',
+      license='MIT',
+      packages=['variational_principle'],
+      zip_safe=False)
