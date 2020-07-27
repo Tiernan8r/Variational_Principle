e8389ee54bb9a27c098d67bfc504af2de79fda05
Tiernan8r <tiernan8r@pm.me>
2020-07-27 17:09:13 +0100

    Change of upstream repo name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='variational_principle',
       version='1.1',
       description='The Variational Principle method for computing bound energy eigenstates.',
-      url='https://github.com/Tiernan8r/Variational-Principle',
+      url='https://github.com/Tiernan8r/Variational_Principle',
       author='Tiernan8r',
       author_email='tiernan8r@pm.me',
       license='MIT',
